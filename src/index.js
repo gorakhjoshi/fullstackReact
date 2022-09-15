86e366b7163853420abbeffc005ea4080389fc3b
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-15 07:17:36 +0545

    added css inside scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import { StrictMode } from "react";
 import App from "./App";
+import "./scss/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
