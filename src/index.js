5e93757f5224235fda7aa71e46aba9cf39883936
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-15 06:40:25 +0545

    refractor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <div>Hello!</div>
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const destination = document.getElementById("root");
+// const sourceCode = <div>Hello World</div>;
+
+// const getDest = ReactDOM.createRoot(destination);
+
+// getDest.render(sourceCode);
