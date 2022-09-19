c69296e326219fced7a7c8008f3f40282ad2aa6f
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-19 07:16:37 +0545

    using react states

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -9,12 +9,23 @@ function AccordionItem({
   return (
     <div className="faq__question" onClick={onClick}>
       <dt>
-        <button className="faq__question-button" aria-expanded="true">
+        <button
+          className={`faq__question-button ${fontBold}`}
+          aria-expanded={ariaExpanded}
+          data-qa="faq__question-button"
+          aria-control={`faq${index + 1}_desc`}
+        >
           {item.question}
         </button>
       </dt>
       <dd>
-        <p className="faq__descOld showDescription">{item.answer}</p>
+        <p
+          className={`faq__desc ${showDescription}`}
+          id={`faq${index + 1}_desc`}
+          data-qa="faq__desc"
+        >
+          {item.answer}
+        </p>
       </dd>
     </div>
   );
