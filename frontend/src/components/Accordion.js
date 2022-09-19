9879c316035fce4eeb24179db2d1c25cd98c25cd
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-09-19 08:35:27 +0545

    data fetched from api

---
+++ frontend/src/components/Accordion.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import AccordionItem from "./AccordionItem";
+
+function Accordion({ questionAnswers }) {
+  const [activeItem, setActiveItem] = useState(0);
+
+  return (
+    <div className="faq">
+      <h1 className="faq__title">FAQ</h1>
+      <dl>
+        {questionAnswers?.map((item, index) => {
+          const showDescription =
+            index === activeItem ? "show-description" : "";
+          const ariaExpanded = index === activeItem ? true : false;
+          const fontBold = index === activeItem ? true : false;
+          return (
+            <AccordionItem
+              item={item}
+              key={index}
+              onClick={() => setActiveItem(index)}
+              index={index}
+              showDescription={showDescription}
+              ariaExpanded={ariaExpanded}
+              fontBold={fontBold}
+            />
+          );
+        })}
+      </dl>
+    </div>
+  );
+}
+
+export default Accordion;
---
+++ frontend/src/components/AccordionItem.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+
+function AccordionItem({
+  item,
+  onClick,
+  showDescription,
+  ariaExpanded,
+  fontBold,
+  index,
+}) {
+  return (
+    <div className="faq__question" onClick={onClick}>
+      <dt>
+        <button
+          className={`faq__question-button ${fontBold}`}
+          aria-expanded={ariaExpanded}
+          // data-qa="faq__question-button"
+          aria-control={`faq${index + 1}_desc`}
+        >
+          {/* faq1_desc */}
+          {/* faq2_desc */}
+          {item.question}
+        </button>
+      </dt>
+      <dd>
+        <p
+          className={`faq__desc ${showDescription}`}
+          id={`faq${index + 1}_desc`}
+          data-qa="faq__desc"
+        >
+          {item.answer}
+        </p>
+      </dd>
+    </div>
+  );
+}
+
+export default AccordionItem;
